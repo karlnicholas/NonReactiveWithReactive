0522810eb108f824b451f0b68db7aeccfb4f5e25
Karl Nicholas <karl.nicholas@outlook.com>
2024-12-30 18:54:21 -0500

    Updates with keycloak security

--- NonReactiveWithReactiveApp/src/test/java/org/example/nonreactivewithreactive/ExternalApiIntegrationIT.java
+++ NonReactiveWithReactiveApp/src/test/java/org/example/nonreactivewithreactive/ExternalApiIntegrationIT.java
@@ -2,6 +2,8 @@ package org.example.nonreactivewithreactive;
 
 import org.example.nonreactivewithreactive.model.Message;
 import org.example.nonreactivewithreactive.service.AppService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -13,6 +15,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MSSQLServerContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
